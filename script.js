64a576330e492b4c9c49a7f68b934459c926775b
superShine1215 <62837477+superShine1215@users.noreply.github.com>
2023-09-19 16:08:29 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+const body = document.body;
+const nextBtn = document.getElementById("next");
+const prevBtn = document.getElementById("prev");
+const slideContents = document.querySelectorAll(".slider-content");
+
+let activeSlide = 0;
+
+function setBgImg() {
+  body.style.backgroundImage = slideContents[activeSlide].style.backgroundImage;
+}
+
+setBgImg();
+
+function setActiveSlide() {
+  slideContents.forEach((x) => {
+    x.classList.remove("active");
+  });
+  slideContents[activeSlide].classList.add("active");
+}
+
+setActiveSlide();
+
+nextBtn.addEventListener("click", () => {
+  nextSlide();
+  setBgImg();
+  setActiveSlide();
+});
+
+prevBtn.addEventListener("click", () => {
+  prevSlide();
+  setBgImg();
+  setActiveSlide();
+});
+
+function nextSlide() {
+  activeSlide++;
+  if (activeSlide > slideContents.length - 1) {
+    activeSlide = 0;
+  }
+}
+
+function prevSlide() {
+  activeSlide--;
+  if (activeSlide < 0) {
+    activeSlide = slideContents.length - 1;
+  }
+}
+
+setInterval(() => {
+  nextSlide();
+  setBgImg();
+  setActiveSlide();
+}, 5000);
